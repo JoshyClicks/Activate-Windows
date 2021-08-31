48266d996ac63f1b87d191edf5386ef080684036
Temp <67669360+JoshyClicks@users.noreply.github.com>
2021-08-31 21:13:55 +0800

    Add files via upload

---
+++ Activate_Windows.cmd
@@ -0,0 +1,21 @@
+Made by jshy
+
+
+
+@echo off
+color b
+@echo This                                                                                                           
+@echo Script                                                                                                         
+@echo Activates                                                                                                      
+@echo Windows                                                                                                        
+@echo :)                                                                                                                                                                                              
+
+pause
+
+slmgr /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX
+
+slmgr /skms kms8.msguides.com
+
+slmgr /ato
+
+
